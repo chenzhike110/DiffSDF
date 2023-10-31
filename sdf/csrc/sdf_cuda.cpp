dccb9c9993afd5729347a45abd6534cc08cf377e
chenzhike110 <3180103610@zju.edu.cn>
2023-10-31 13:01:51 +0800

    update code

---
+++ sdf/csrc/sdf_cuda.cpp
@@ -0,0 +1,31 @@
+#include <torch/torch.h>
+
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+// CUDA forward declarations
+
+at::Tensor sdf_cuda(
+        at::Tensor phi,
+        at::Tensor faces,
+        at::Tensor vertices,
+        at::Tensor query);
+
+at::Tensor sdf(
+        at::Tensor phi,
+        at::Tensor faces,
+        at::Tensor vertices,
+        at::Tensor query) {
+
+	CHECK_INPUT(phi);
+	CHECK_INPUT(faces);
+	CHECK_INPUT(vertices);
+        CHECK_INPUT(query);
+
+	return sdf_cuda(phi, faces, vertices, query);
+};
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("sdf", &sdf, "SDF (CUDA)");
+}
---
+++ sdf/sdf.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+
+import sdf.csrc as _C
+
+class SDFFunction(Function):
+    """
+    Definition of SDF function
+    """
+
+    @staticmethod
+    def forward(ctx, phi, faces, vertices, query):
+        return _C.sdf(phi, faces, vertices, query)
+
+    @staticmethod
+    def backward(ctx):
+        return None, None, None
+
+class SDF(nn.Module):
+
+    def forward(self, faces, vertices, query):
+        phi = torch.zeros(query.shape[0], query.shape[1], query.shape[2]+1, device=vertices.device)
+        phi = SDFFunction.apply(phi, faces, vertices, query)
+        return phi
